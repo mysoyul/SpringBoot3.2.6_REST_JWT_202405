a04068f551a9140f7a39be7fadccf3e1feb65d2f
mysoyul <vega2k_@naver.com>
2024-05-27 16:34:15 +0900

    LectureValidator add

--- src/main/java/com/boot3/myrestapi/common/errors/ErrorsSerializer.java
+++ src/main/java/com/boot3/myrestapi/common/errors/ErrorsSerializer.java
@@ -14,6 +14,7 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
 	@Override
 	public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
 		gen.writeStartArray();
+        //Iterable forEach(Consumer) Consumer 의 추상메서드 void accept(T t)
         errors.getFieldErrors().forEach(e -> {
             try {
                 gen.writeStartObject();
