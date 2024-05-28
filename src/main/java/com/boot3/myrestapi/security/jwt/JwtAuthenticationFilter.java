47b70e080da0fa38d28f761a19ca453dd5dddbdb
mysoyul <vega2k_@naver.com>
2024-05-28 17:52:22 +0900

    email userId replace

--- src/main/java/com/boot3/myrestapi/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/boot3/myrestapi/security/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.boot3.myrestapi.security.jwt;
 
+import com.boot3.myrestapi.security.userinfo.UserInfo;
+import com.boot3.myrestapi.security.userinfo.UserInfoRepository;
 import com.boot3.myrestapi.security.userinfo.UserInfoUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,6 +25,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private UserInfoUserDetailsService userDetailsService;
 
+    @Autowired
+    private UserInfoRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -32,16 +38,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         //Bearer eyJhbGciOiJIUzI1NiJ9.ey
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
-            username = jwtService.extractUsername(token);
+            //username = jwtService.extractUsername(token);
+            String userId = jwtService.extractUsername(token);
+            UserInfo userInfo = repository.findByUserId(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("user not found "));
+            username = userInfo.getEmail();
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            if (jwtService.validateToken(token, userDetails)) {
+            //UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            //if (jwtService.validateToken(token, userDetails)) {
+            if (jwtService.validateToken(token)) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails,
                                 null, userDetails.getAuthorities());
+                //Authentication 객체생성
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                //Authentication 객체를 SecurityContext에 저장함
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
